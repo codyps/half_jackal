aced5ca19ff330f0392c494050855cefc38955ab
Cody Schafer <cpschafer@gmail.com>
2011-01-13 01:19:18 -0500

    add hj specific protocol headers.

---
+++ hj_proto.h
@@ -0,0 +1,65 @@
+#ifndef HJ_PROTO_H_
+#define HJ_PROTO_H_
+
+#include <stdint.h>
+#ifndef __packed
+# define __packed __attribute__((packed))
+#endif
+
+
+
+
+/** packet components **/
+
+/* sent at the start of each packet */
+struct hj_pktc_header {
+	uint8_t type;
+} __packed;
+
+struct hj_pktc_motor_info {
+	uint16_t current;
+	uint32_t enc_ct;
+	uint16_t cur_vel;
+} __packed;
+
+/** packets dispatched TO the hj. **/
+struct hj_pkt_set_speed {
+	struct hj_pktc_header head;
+	/* "vel" of both attached motors, TODO: determine units. */
+	int16_t vel;
+	uint16_t crc; /* the CCITT crc */
+} __packed;
+
+/** packets returned FROM the hj. **/
+struct hj_pkt_info {
+	struct hj_pktc_header head;
+	struct hj_pktc_motor_info a, b;
+	uint16_t crc;
+} __packed;
+
+struct hj_pkt_timeout {
+	struct hj_pktc_header head;
+	uint16_t crc;
+} __packed;
+
+/** **/
+
+enum hj_pkt_len {
+	HJ_PL_TIMEOUT = sizeof(struct hj_pkt_timeout),
+	HJ_PL_SET_SPEED = sizeof(struct hj_pkt_set_speed),
+	HJ_PL_INFO = sizeof(struct hj_pkt_info)
+};
+
+#define HJ_PKT_MIN_SZ HJ_PL_TIMEOUT
+#define HJ_PKT_MAX_SZ HJ_PL_INFO
+
+enum hj_pkt_type {
+	HJ_PT_TIMEOUT = 0,
+	HJ_PT_SET_SPEED,
+	HJ_PT_INFO
+};
+
+#define HJ_PKT_TIMOUT_INITIALIZER { .head = { .type = HJ_PT_TIMEOUT },\
+	.crc = htons(0x1d0f) }
+
+#endif
