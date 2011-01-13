3c5959b208f747bfd84a97030ae4d083b4959eca
John Mesmon <jmesmon@gmail.com>
2011-01-13 06:13:51 -0500

    avr side: basic hj_proto support, no speed control used.

--- avr/main.c
+++ avr/main.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <util/delay.h>
+#include <util/crc16.h>
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -7,8 +8,60 @@
 #include "error_led.h"
 #include "frame_async.h"
 
+
 #include "muc/muc.h"
 #include "motor_shb.h"
+#include "../hj_proto.h"
+
+static void hj_motor_set_speed(int16_t speed) {
+	mshb_set(0, speed);
+	mshb_set(1, speed);
+}
+
+static inline void hj_parse(uint8_t *buf, uint8_t len)
+{
+	if (len < HJ_PL_MIN) {
+		return;
+	}
+
+	{
+		/* compute CRC over the length of the packet minus the
+		 * crc at the end */
+		uint16_t crc = 0xffff;
+		uint8_t i;
+		for (i = 0; i < (len - 2); i++) {
+			crc = _crc_ccitt_update(crc, buf[i]);
+		}
+
+		uint16_t recv_crc = (buf[i] << 8) | buf[i+1];
+
+		if (crc != recv_crc)
+			return;
+	}
+
+	struct hj_pktc_header *head = (typeof(head)) buf;
+
+	switch(head->type) {
+	case HJ_PT_SET_SPEED:
+		if (len != HJ_PL_SET_SPEED) {
+			return;
+		}
+
+		struct hj_pkt_set_speed *pkt = (typeof(pkt)) buf;
+
+		hj_motor_set_speed(ntohs(pkt->vel));
+
+		break;
+	case HJ_PT_REQ_INFO:
+		if (len != HJ_PL_REQ_INFO) {
+			return;
+		}
+
+		/* send info */
+		break;
+	}
+}
+
 
 __attribute__((noreturn))
 void main(void)
@@ -19,20 +72,18 @@ void main(void)
 	led_init();
 	sei();
 	for(;;) {
-		uint8_t buf[16];
+		uint8_t buf[HJ_PL_MAX];
 		uint8_t len = frame_recv_copy(buf, sizeof(buf));
 		if (len) {
-			led_flash(5);
-			ct = 0;
-			frame_send(buf, MIN(len, sizeof(buf)));
-			frame_recv_next();
+			hj_parse(buf, len);
 		} else {
 			ct++;
 			if (ct == 0) {
 				ct++;
 				led_flash(3);
-				const char rdy_str[] = "hello";
-				frame_send(rdy_str, strlen(rdy_str));
+				struct hj_pkt_timeout tout
+					= HJ_PKT_TIMEOUT_INITIALIZER;
+				frame_send(&tout, HJ_PL_TIMEOUT);
 			}
 			_delay_ms(70);
 		}
--- hj_proto.h
+++ hj_proto.h
@@ -6,9 +6,6 @@
 # define __packed __attribute__((packed))
 #endif
 
-
-
-
 /** packet components **/
 
 /* sent at the start of each packet */
@@ -30,6 +27,11 @@ struct hj_pkt_set_speed {
 	uint16_t crc; /* the CCITT crc */
 } __packed;
 
+struct hj_pkt_req_info {
+	struct hj_pktc_header head;
+	uint16_t crc;
+} __packed;
+
 /** packets returned FROM the hj. **/
 struct hj_pkt_info {
 	struct hj_pktc_header head;
@@ -42,24 +44,27 @@ struct hj_pkt_timeout {
 	uint16_t crc;
 } __packed;
 
+
 /** **/
 
 enum hj_pkt_len {
 	HJ_PL_TIMEOUT = sizeof(struct hj_pkt_timeout),
 	HJ_PL_SET_SPEED = sizeof(struct hj_pkt_set_speed),
-	HJ_PL_INFO = sizeof(struct hj_pkt_info)
+	HJ_PL_INFO = sizeof(struct hj_pkt_info),
+	HJ_PL_REQ_INFO = sizeof(struct hj_pkt_req_info)
 };
 
-#define HJ_PKT_MIN_SZ HJ_PL_TIMEOUT
-#define HJ_PKT_MAX_SZ HJ_PL_INFO
+#define HJ_PL_MIN HJ_PL_TIMEOUT
+#define HJ_PL_MAX HJ_PL_INFO
 
 enum hj_pkt_type {
 	HJ_PT_TIMEOUT = 0,
 	HJ_PT_SET_SPEED,
+	HJ_PT_REQ_INFO,
 	HJ_PT_INFO
 };
 
-#define HJ_PKT_TIMOUT_INITIALIZER { .head = { .type = HJ_PT_TIMEOUT },\
+#define HJ_PKT_TIMEOUT_INITIALIZER { .head = { .type = HJ_PT_TIMEOUT },\
 	.crc = htons(0x1d0f) }
 
 #endif
