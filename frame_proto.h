52feeb4caf874bd5c9b4675df6b85afb7ed7a488
Cody Schafer <cpschafer@gmail.com>
2011-01-18 23:43:39 -0500

    frame: add includes.

--- frame_proto.h
+++ frame_proto.h
@@ -1,7 +1,10 @@
 #ifndef PROTO_H_
 #define PROTO_H_
 
+#include <stdint.h>
+
 #define CRC_INIT 0xffff
+#define CRC_SZ sizeof(uint16_t)
 
 #define START_BYTE ((uint8_t)0x7e)
 #define RESET_BYTE ((uint8_t)0x7f)
