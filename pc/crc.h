0ca1670bb0e6b2400a2922a401bd72813d54d48d
Cody Schafer <cpschafer@gmail.com>
2011-01-20 18:58:46 -0500

    pc/crc: fixups for build.

--- pc/crc.h
+++ pc/crc.h
@@ -1,6 +1,21 @@
 #ifndef CRC_H_
 #define CRC_H_
 
+#include <stdint.h>
+
+#if 1
+static inline
+uint8_t lo8(uint16_t val)
+{
+	return val & 0xff;
+}
+
+static inline
+uint8_t hi8(uint16_t val)
+{
+	return val >> 8;
+}
+
 static inline
 uint16_t crc_ccitt_update(uint16_t crc, uint8_t data)
 {
@@ -10,5 +25,18 @@ uint16_t crc_ccitt_update(uint16_t crc, uint8_t data)
 	return ((((uint16_t) data << 8) | hi8(crc)) ^ (uint8_t) (data >> 4)
 		^ ((uint16_t) data << 3));
 }
+#else
+
+static inline
+uint16_t crc_ccitt_update(uint16_t crc, uint8_t data)
+{
+	crc  = (uint8_t)(crc >> 8) | (crc << 8);
+	crc ^= data;
+	crc ^= (uint8_t)(crc & 0xff) >> 4;
+	crc ^= (crc << 8) << 4;
+	crc ^= ((crc & 0xff) << 4) << 1;
+	return crc;
+}
+#endif
 
 #endif
