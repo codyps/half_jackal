9c47feb586f670d67c342ca67d2f6c7bad04313c
Cody Schafer <cpschafer@gmail.com>
2011-01-20 18:59:29 -0500

    pc: update for testing.

--- pc/main.c
+++ pc/main.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 
 #include "frame_async.h"
+#include "../hj_proto.h"
+
 
 struct bytebuf {
 	char *buf;
@@ -30,29 +32,36 @@ struct bytebuf *bytebuf_append(struct bytebuf *s, char c) {
 	return 0;
 }
 
+
+void send_req_info(FILE *out)
+{
+	struct hj_pkt_req_info ri = HJ_PKT_REQ_INFO_INITIALIZER;
+	frame_send(out, &ri, HJ_PL_REQ_INFO);
+}
+
 int main(int argc, char **argv)
 {
-
-	char *d = "~";
+	send_req_info(stdout);
+	return 1;
 	char buf[1024];
-	frame_send(stdout, d, strlen(d));
-	frame_recv(stdin, buf, sizeof(buf));
-
-	fprintf(stderr, "%s\n", buf);
-#if 0
-	int c;
 	for(;;) {
-		while((c = fgetc(stdin)) != EOF) {
+		size_t len = frame_recv(stdin, buf, sizeof(buf));
+		struct hj_pktc_header *h = (typeof(h)) buf;
 
-
+		switch(h->type) {
+		case HJ_PT_TIMEOUT: {
+			fprintf(stderr, "recieved timeout packet\n");
+			send_req_info(stdout);
+			break;
 		}
-		if (ferror(stdin)) {
-			exit(-2);
-		} else if (feof(stdin)) {
-			clearerr(stdin);
-			send_packet(
+		default: {
+			fprintf(stderr, "recieved unknown pt %x, len %zu\n",
+					h->type, len );
+			struct hj_pkt_req_info ri = HJ_PKT_REQ_INFO_INITIALIZER;
+			frame_send(stdout, &ri, HJ_PL_REQ_INFO);
+			break;
+		}
 		}
 	}
-#endif
 	return 0;
 }
