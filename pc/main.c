c469e059d1ca078afd460da4913a0c4586837f45
Cody Schafer <cpschafer@gmail.com>
2011-01-23 01:27:37 -0500

    pc: support INFO packets.

--- pc/main.c
+++ pc/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <inttypes.h>
+
+#include <arpa/inet.h>
 
 #include "frame_async.h"
 #include "../hj_proto.h"
@@ -39,26 +42,58 @@ void send_req_info(FILE *out)
 	frame_send(out, &ri, HJ_PL_REQ_INFO);
 }
 
+void print_hj_motor_info(struct hj_pktc_motor_info *inf, FILE *out)
+{
+	fprintf(out, "current: %"PRIu16" enc_ct: %"PRIu32
+			" cur_vel: %"PRIu16"",
+			ntohs(inf->current),
+			ntohl(inf->enc_ct),
+			ntohs(inf->cur_vel));
+}
+
 int main(int argc, char **argv)
 {
-	send_req_info(stdout);
-	return 1;
 	char buf[1024];
 	for(;;) {
 		size_t len = frame_recv(stdin, buf, sizeof(buf));
 		struct hj_pktc_header *h = (typeof(h)) buf;
 
+		if (len < 0) {
+			fprintf(stderr, "frame_recv => %zu\n", len);
+			continue;
+		}
+
 		switch(h->type) {
 		case HJ_PT_TIMEOUT: {
-			fprintf(stderr, "recieved timeout packet\n");
+			fprintf(stderr, "HJ_PT_TIMEOUT:");
+			if (len != HJ_PL_TIMEOUT) {
+				fprintf(stderr, "len = %zu, expected %d\n",
+						len, HJ_PL_TIMEOUT);
+				continue;
+			}
+			fputc('\n', stderr);
 			send_req_info(stdout);
+			break;
+		}
+		case HJ_PT_INFO: {
+			fprintf(stderr, "HJ_PT_INFO:");
+			if (len != HJ_PL_INFO) {
+				fprintf(stderr,	" len = %zu, expected %d\n",
+						len, HJ_PL_INFO);
+				continue;
+			}
+			struct hj_pkt_info *inf = (typeof(inf)) buf;
+			fprintf(stderr, "\n\ta: ");
+			print_hj_motor_info(&inf->a, stderr);
+			fprintf(stderr, "\n\tb: ");
+			print_hj_motor_info(&inf->b, stderr);
+			fprintf(stderr, "\n");
+
 			break;
 		}
 		default: {
 			fprintf(stderr, "recieved unknown pt %x, len %zu\n",
 					h->type, len );
-			struct hj_pkt_req_info ri = HJ_PKT_REQ_INFO_INITIALIZER;
-			frame_send(stdout, &ri, HJ_PL_REQ_INFO);
 			break;
 		}
 		}
