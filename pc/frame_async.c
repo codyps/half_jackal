13837dc3892149512044fad5f7504ee812338c3f
Cody Schafer <cpschafer@gmail.com>
2011-02-14 07:22:27 -0500

    need non-blocking...

--- pc/frame_async.c
+++ pc/frame_async.c
@@ -42,6 +42,43 @@ ssize_t frame_send(FILE *out, void *data, size_t nbytes)
 	return nbytes;
 }
 
+#if 0
+struct frame_ctx_i {
+	bool start;
+	bool esc;
+	uint16_t crc;
+};
+
+struct frame_ctx {
+	int fd;
+	struct frame_ctx_i send, recv;
+};
+
+void frame_open(struct frame_ctx *fc, int fd)
+{
+	fc->fd = fd;
+	fc->send.start = false;
+	fc->send.esc = false;
+	fc->send.crc = CRC_INIT;
+
+	fc->recv.start = false;
+	fc->recv.esc = false;
+	fc->recv.crc = CRC_INIT;
+}
+
+ssize_t frame_send(struct frame_ctx *fc, void *data, size_t nbytes)
+{
+	if (fc->
+	ssize_t x = write(fc->fd,
+}
+
+ssize_t frame_recv(struct frame_ctx *fc, void *vbuf, size_t nbytes)
+{
+
+}
+#endif
+
+
 ssize_t frame_recv(FILE *in, void *vbuf, size_t nbytes)
 {
 	uint16_t crc = CRC_INIT;
