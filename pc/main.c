106d9c1cb9ff605acbf97905c56664d76af63aaf
Cody Schafer <cpschafer@gmail.com>
2011-03-05 02:51:21 -0500

    pc: init serial.

--- pc/main.c
+++ pc/main.c
@@ -4,6 +4,14 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <termios.h>
+#include <unistd.h>
+
 #include <arpa/inet.h>
 
 #include "frame_async.h"
@@ -52,30 +60,102 @@ void print_hj_motor_info(struct hj_pktc_motor_info *inf, FILE *out)
 			(int16_t)ntohs(inf->vel));
 }
 
+#define ERROR(fmt, ...) _ERROR(__FILE__,__LINE__,__func__,fmt,##__VA_ARGS__)
+
+#define _ERROR(file, line, func, fmt, ...) \
+	__ERROR(file, line, func, fmt, ##__VA_ARGS__)
+#define __ERROR(file, line, func, fmt, ...)  do {	\
+	fputs(file ":" #line ":", stderr);	\
+	fprintf(stderr, fmt, ##__VA_ARGS__);		\
+	fputc('\n',stderr);				\
+} while(0)
+
+static int serial_conf(int fd, speed_t speed)
+{
+	return 0;
+	struct termios t;
+	int ret = tcgetattr(fd, &t);
+
+	if (ret < 0)
+		return ret;
+
+	ret = cfsetispeed(&t, speed);
+	if (ret < 0)
+		return ret;
+
+	ret = cfsetospeed(&t, speed);
+	if (ret < 0)
+		return ret;
+
+	/* odd parity */
+	t.c_cflag |= PARENB | PARODD;
+
+	/* 8 data bits */
+	t.c_cflag = (t.c_cflag & ~CSIZE) | CS8;
+
+	/* no flow control */
+	t.c_cflag &= ~(CRTSCTS);
+	t.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+	/* ignore control lines */
+	t.c_cflag |= CLOCAL;
+
+	return tcsetattr(fd, TCSAFLUSH, &t);
+}
+
+static FILE *serial_open(char const *fname)
+{
+	int sfd = open(fname, O_RDWR);
+	if (sfd < 0) {
+		ERROR("open: %s: %s", fname, strerror(errno));
+		return NULL;
+	}
+
+	int ret = serial_conf(sfd, B57600);
+	if (ret < 0) {
+		ERROR("serial_conf: %s: %s", fname, strerror(errno));
+		return NULL;
+	}
+
+	FILE *sf = fdopen(sfd, "a+");
+	if (!sf) {
+		ERROR("fdopen: %s: %s", fname, strerror(errno));
+		return NULL;
+	}
+
+	return sf;
+}
+
 int main(int argc, char **argv)
 {
-	if (argc < 3) {
-		fprintf(stderr, "usage: %s <motor a> <motor b>\n",
+	if (argc < 4) {
+		fprintf(stderr, "usage: %s <file> <motor a> <motor b>\n",
 				argc?argv[0]:"hj");
 		return -1;
 	}
 
+	FILE *sf = serial_open(argv[1]);
+	if (!sf) {
+		ERROR("open: %s", strerror(errno));
+		return -1;
+	}
+
 	int motors[2];
-	int ret = sscanf(argv[1], "%x", &motors[0]);
+	int ret = sscanf(argv[2], "%x", &motors[0]);
 	if (ret != 1) {
-		fprintf(stderr, "not a number: \"%s\"", argv[1]);
+		ERROR("not a number: \"%s\"", argv[1]);
 		return -2;
 	}
 
-	ret = sscanf(argv[2], "%x", &motors[1]);
+	ret = sscanf(argv[3], "%x", &motors[1]);
 	if (ret != 1) {
-		fprintf(stderr, "not a number: \"%s\"", argv[2]);
+		ERROR("not a number: \"%s\"", argv[2]);
 		return -2;
 	}
 
 	char buf[1024];
 	for(;;) {
-		size_t len = frame_recv(stdin, buf, sizeof(buf));
+		size_t len = frame_recv(sf, buf, sizeof(buf));
 		struct hj_pktc_header *h = (typeof(h)) buf;
 
 		if (len < 0) {
@@ -92,7 +172,7 @@ int main(int argc, char **argv)
 				continue;
 			}
 			fputc('\n', stderr);
-			send_req_info(stdout);
+			send_req_info(sf);
 			break;
 		}
 		case HJA_PT_INFO: {
@@ -113,7 +193,7 @@ int main(int argc, char **argv)
 				HJB_PKT_SET_SPEED_INITIALIZER(motors[0],
 						motors[1]);
 
-			frame_send(stdout, &ss, HJB_PL_SET_SPEED);
+			frame_send(sf, &ss, HJB_PL_SET_SPEED);
 
 			break;
 		}
