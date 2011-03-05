0fbc348a7f5f976a44a4174cb10fe6494c2e346b
Cody Schafer <cpschafer@gmail.com>
2011-03-05 03:02:28 -0500

    pc/term: remove prints.

--- pc/term.c
+++ pc/term.c
@@ -9,8 +9,6 @@
 
 #include <string.h>
 
-#include "error_m.h"
-
 static int serial_conf(int fd, speed_t speed)
 {
 	return 0;
@@ -48,19 +46,16 @@ FILE *term_open(char const *fname)
 {
 	int sfd = open(fname, O_RDWR);
 	if (sfd < 0) {
-		ERROR("open: %s: %s", fname, strerror(errno));
 		return NULL;
 	}
 
 	int ret = serial_conf(sfd, B57600);
 	if (ret < 0) {
-		ERROR("serial_conf: %s: %s", fname, strerror(errno));
 		return NULL;
 	}
 
 	FILE *sf = fdopen(sfd, "a+");
 	if (!sf) {
-		ERROR("fdopen: %s: %s", fname, strerror(errno));
 		return NULL;
 	}
 
