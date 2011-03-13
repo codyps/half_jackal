b1587d7ef81a0360b733531ee0ec08e8bdf6b5d0
Cody Schafer <cpschafer@gmail.com>
2011-03-13 01:36:40 -0500

    remove use of the second pwm per Adam Stambler's advice.

--- avr/motor_shb.h
+++ avr/motor_shb.h
@@ -15,23 +15,16 @@ struct pwm16_out {
 	struct pin p;
 };
 
-struct pwm8_out {
-	uint8_t volatile *mreg;
-	struct pin p;
-};
-
 struct mshb {
-	/* the atmega328p only has 2 16bit pwms, so we sacrafice pwm
-	 * accuracy in the reverse */
 	struct pwm16_out pwma;
-	struct pwm8_out pwmb;
+	struct pin b;
 	struct pin enable;
 };
 
 #define PIN_INITIALIZER(port_, idx) { .port = &(port_), .mask = (1 << (idx)) }
 #define PWM_INITIALIZER(reg, pin) { .mreg = &(reg), .p = pin }
 #define MSHB_INITIALIZER(pa, pb, en)				\
-		{ .pwma = pa, .pwmb = pb, .enable = en }
+		{ .pwma = pa, .b = pb, .enable = en }
 
 #define PD_4 PIN_INITIALIZER(PORTD, 4) // arduino  4
 #define PD_5 PIN_INITIALIZER(PORTD, 5) // arduino  5
@@ -52,8 +45,8 @@ struct mshb {
 #define TMR1_PWMB PWM_INITIALIZER(OCR1B, PB_2)
 
 static struct mshb mshb_d [] = {
-	MSHB_INITIALIZER(TMR1_PWMA, TMR0_PWMA, PD_4), // A= 9, B=6, INH=4
-	MSHB_INITIALIZER(TMR1_PWMB, TMR0_PWMB, PD_7)  // A=10, B=5, INH=7
+	MSHB_INITIALIZER(TMR1_PWMA, PD_6, PD_4), // A= 9, B=6, INH=4
+	MSHB_INITIALIZER(TMR1_PWMB, PD_5, PD_7)  // A=10, B=5, INH=7
 };
 
 #define PIN_INIT_OUT(pin) do {					\
@@ -75,15 +68,9 @@ static struct mshb mshb_d [] = {
 
 #define MSHB_INIT(mshb) do {					\
 	PWM_INIT((mshb).pwma);					\
-	PWM_INIT((mshb).pwmb);					\
 	PIN_INIT_OUT((mshb).enable);				\
+	PIN_INIT_OUT((mshb).b);					\
 } while(0)
-
-static inline
-void pwm8_set(struct pwm8_out pwm, uint16_t val15)
-{
-	*(pwm.mreg) = (uint8_t)((uint16_t)val15 >> (15-8));
-}
 
 static inline
 void pwm16_set(struct pwm16_out pwm, uint16_t val15)
@@ -99,7 +86,6 @@ void mshb_init(void)
 	 * Digital  9 / OC1A / PB1 => PB / PWMB / IN (B)
 	 * Digital  7 / PD7 => ENA / ENB / INH (A) / INH (B)
 	 */
-	TIMER0_INIT_PWM_MAX();
 	TIMER1_INIT_PWM(INT16_MAX);
 	uint8_t i;
 	for (i = 0; i < ARRAY_SIZE(mshb_d); i++) {
@@ -123,11 +109,11 @@ static inline
 void mshb_set(uint8_t i, int16_t speed)
 {
 	if (speed < 0) {
-		pwm16_set(mshb_d[i].pwma, 0);
-		pwm8_set(mshb_d[i].pwmb, -speed);
+		pwm16_set(mshb_d[i].pwma, INT16_MAX - speed);
+		PIN_SET_HIGH(mshb_d[i].b);
 	} else {
-		pwm8_set(mshb_d[i].pwmb, 0);
 		pwm16_set(mshb_d[i].pwma, speed);
+		PIN_SET_LOW(mshb_d[i].b);
 	}
 }
 
