d62a54f2ee27f756cf90be03fbbffb909d8fb8de
Cody Schafer <cpschafer@gmail.com>
2011-03-23 23:32:07 -0500

    pc: move parsing out of main.

--- pc/main.c
+++ pc/main.c
@@ -66,34 +66,8 @@ void print_hj_motor_info(struct hj_pktc_motor_info *inf, FILE *out)
 			(int16_t)ntohs(inf->vel));
 }
 
-int main(int argc, char **argv)
+void hj_parse(FILE *sf, int16_t motors[2])
 {
-	if (argc < 4) {
-		fprintf(stderr, "usage: %s <file> <motor a> <motor b>\n",
-				argc?argv[0]:"hj");
-		return -1;
-	}
-
-	FILE *sf = term_open(argv[1]);
-	if (!sf) {
-		ERROR("open: %s", strerror(errno));
-		return -1;
-	}
-
-	int motors[2];
-	int ret = sscanf(argv[2], "%x", &motors[0]);
-	if (ret != 1) {
-		ERROR("not a number: \"%s\"", argv[1]);
-		return -2;
-	}
-
-	ret = sscanf(argv[3], "%x", &motors[1]);
-	if (ret != 1) {
-		ERROR("not a number: \"%s\"", argv[2]);
-		return -2;
-	}
-
-
 	char buf[1024];
 	for(;;) {
 		ssize_t len = frame_recv(sf, buf, sizeof(buf));
@@ -155,5 +129,36 @@ int main(int argc, char **argv)
 		}
 		}
 	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 4) {
+		fprintf(stderr, "usage: %s <file> <motor a> <motor b>\n",
+				argc?argv[0]:"hj");
+		return -1;
+	}
+
+	FILE *sf = term_open(argv[1]);
+	if (!sf) {
+		ERROR("open: %s", strerror(errno));
+		return -1;
+	}
+
+	int16_t motors[2];
+	int ret = sscanf(argv[2], "%"SCNx16, &motors[0]);
+	if (ret != 1) {
+		ERROR("not a number: \"%s\"", argv[1]);
+		return -2;
+	}
+
+	ret = sscanf(argv[3], "%"SCNx16, &motors[1]);
+	if (ret != 1) {
+		ERROR("not a number: \"%s\"", argv[2]);
+		return -2;
+	}
+
+	hj_parse(sf, motors);
+
 	return 0;
 }
