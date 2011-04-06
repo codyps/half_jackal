a05cb1214aa76091d147887d709e0c3ac5a85365
Cody Schafer <cpschafer@gmail.com>
2011-04-06 18:57:00 -0400

    more unix.

--- pc/unix.c
+++ pc/unix.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/un.h>
 
 #include <unistd.h>
@@ -11,6 +12,10 @@
 
 static char const *optstring = ":f:";
 
+struct conn {
+	int fd;
+};
+
 static char *lstrerror(int errnum)
 {
 	static char str[1024];
@@ -19,10 +24,16 @@ static char *lstrerror(int errnum)
 	return str;
 }
 
+static void term_handler(int sig, siginfo_t *inf, void *d)
+{
+	
+}
+
 int main(int argc, char **argv)
 {
 	char *ufile = NULL;
 	int c, errflg = 0;
+	int ret;
 
 	while ((c = getopt(argc, argv, optstring)) != -1) {
 		switch(c) {
@@ -48,19 +59,61 @@ int main(int argc, char **argv)
 
 	if (errflg) {
 		fputs("errors parsing args, exiting.\n", stderr);
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 
 	if (!ufile) {
 		fputs("a filename (-f) is required\n", stderr);
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 
-	int unix_socket = socket(AF_UNIX, SOCK_RDM, 0);
+	struct sigaction term_sa;
+	term_sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&term_sa.sa_mask);
+	term_sa.sa_sigaction = term_handler;
 
+	ret = sigaction(SIG_TERM, &term_sa, NULL);
+	if (ret == -1) {
+		fprintf(stderr, "registering term handler failed.");
+		exit(2);
+	}
+
+
+	int unix_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
 	if (unix_socket == -1) {
-		fprintf(stderr, "not able to create socket: %s\n",
+		fprintf(stderr, "socket: %s\n",
 				lstrerror(errno));
+	}
+
+	struct sockaddr_un sun;
+	sun.sun_family = AF_UNIX;
+	strncpy(sun.sun_path, ufile, sizeof(sun.sun_path));
+	ret = bind(unix_socket, (struct sockaddr const*)&sun, sizeof(sun));
+	if (ret == -1) {
+		fprintf(stderr, "bind: %s\n",
+				lstrerror(errno));
+	}
+
+	ret = listen(unix_socket, 0xf);
+	if (ret == -1) {
+		fprintf(stderr, "listen: %s\n",
+				lstrerror(errno));
+	}
+
+	for(;;) {
+		struct sockaddr_un sunp;
+		socklen_t sl = sizeof(sunp);
+		int s = accept(unix_socket, (struct sockaddr *)&sunp, &sl);
+		if (s == -1) {
+			fprintf(stderr, "accept: %s\n",
+					lstrerror(errno));
+			exit(2);
+		}
+
+		char *msg = "hello";
+		write(s, msg, strlen(msg));
+
+		close(s);
 	}
 
 	return 0;
